81f3dca2936db4e9b12d6ad064d16adfa0456985
Joshuak52 <joshuak5222@gmail.com>
2023-10-05 18:51:49 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,26 +7,25 @@ const foods = [];
 let mouseX = 0; // Mouse X coordinate
 let mouseY = 0; // Mouse Y coordinate
 
-// Generate random positions
-function randomPosition() {
-    return {
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight
-    };
-}
+function initializeGame() {
+    // Generate random positions
+    function randomPosition() {
+        return {
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * window.innerHeight
+        };
+    }
 
-// Create food cells
-for (let i = 0; i < 250; i++) {
-    const food = document.createElement('div');
-    food.className = 'food';
-    const position = randomPosition();
-    food.style.left = `${position.x}px`;
-    food.style.top = `${position.y}px`;
-    canvas.appendChild(food);
-    foods.push(food);
-
-    // Adding random delay factor for food animation
-    food.style.setProperty('--animation-delay-factor', Math.random());
+    // Create food cells
+    for (let i = 0; i < 250; i++) {
+        const food = document.createElement('div');
+        food.className = 'food';
+        const position = randomPosition();
+        food.style.left = `${position.x}px`;
+        food.style.top = `${position.y}px`;
+        canvas.appendChild(food);
+        foods.push(food);
+    }
 }
 
 document.addEventListener('mousemove', (event) => {
@@ -41,7 +40,7 @@ function animate() {
     const deltaX = mouseX - currentX;
     const deltaY = mouseY - currentY;
 
-    player.style.left = `${currentX + deltaX * 0.75}px`; // Adjusted for 25% slower movement
+    player.style.left = `${currentX + deltaX * 0.75}px`;
     player.style.top = `${currentY + deltaY * 0.75}px`;
 
     // Check for eating food
@@ -55,12 +54,10 @@ function animate() {
             const foodY = food.offsetTop + food.offsetHeight / 2;
             const distance = Math.sqrt(Math.pow(playerCenterX - foodX, 2) + Math.pow(playerCenterY - foodY, 2));
 
-            // Check collision with food cells
             if (distance < (player.offsetWidth / 2 + food.offsetWidth / 2)) {
                 canvas.removeChild(food);
                 foods[i] = null;
 
-                // Increase player size after eating food
                 const newSize = Math.min(player.offsetWidth + 1, window.innerWidth / 5);
                 player.style.width = `${newSize}px`;
                 player.style.height = `${newSize}px`;
@@ -70,8 +67,6 @@ function animate() {
 
     requestAnimationFrame(animate);
 }
-
-requestAnimationFrame(animate);
 
 function copyToClipboard() {
     const ipBox = document.getElementById("serverIp");
@@ -88,4 +83,8 @@ document.getElementById('startGame').addEventListener('click', function(event) {
     document.querySelector('.container .instruction').style.display = 'none';
     document.getElementById('startGame').style.display = 'none';
     document.querySelectorAll('.container .discord-button').forEach(btn => btn.style.display = 'none');
+
+    // Initialize the game and start the animate function when the "Start Game" button is clicked
+    initializeGame();
+    requestAnimationFrame(animate);
 });
