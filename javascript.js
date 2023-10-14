1d5317e73a14a9e7855fa9504bafb1f051d8330c
Joshuak52 <joshuak5222@gmail.com>
2023-10-14 15:52:00 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,12 @@
 for (let i = 0; i < 250; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
-
-    const xStart = (Math.random() - 0.5) * 100; // Random X start
-    const yStart = (Math.random() - 0.5) * 100; // Random Y start
-    const xEnd = (Math.random() - 0.5) * 100; // Random X end
-    const yEnd = (Math.random() - 0.5) * 100; // Random Y end
-
-    const animationName = `floatDot${i}`;
-
-    const keyframes = `
-        @keyframes ${animationName} {
-            from {
-                transform: translate(${xStart}px, ${yStart}px);
-            }
-            to {
-                transform: translate(${xEnd}px, ${yEnd}px);
-            }
-        }
-    `;
-
-    const style = document.createElement('style');
-    style.innerHTML = keyframes;
-    document.head.appendChild(style);
+    dot.style.setProperty('--delay', Math.random() * 5); // Add animation delay
 
     dot.style.left = `${Math.random() * 100}vw`;
     dot.style.top = `${Math.random() * 100}vh`;
-    dot.style.animation = `${animationName} 2.5s infinite alternate`; // Halved the animation time to 2.5s
-    dot.style.animationDelay = `${Math.random() * 5}s`;
 
-    document.body.appendChild(dot);
+    document.querySelector('.dots').appendChild(dot);
 }
 
 function copyToClipboard() {
