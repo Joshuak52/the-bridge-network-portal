68b7edc3a15b159e29a109310fdc99f2fe0d46ca
Joshuak52 <joshuak5222@gmail.com>
2023-10-14 19:57:14 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
-// JavaScript code for creating floating dots
 for (let i = 0; i < 250; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
 
+    const xStart = (Math.random() - 0.5) * 100; // Random X start
+    const yStart = (Math.random() - 0.5) * 100; // Random Y start
+    const xEnd = (Math.random() - 0.5) * 100; // Random X end
+    const yEnd = (Math.random() - 0.5) * 100; // Random Y end
+
     const animationName = `floatDot${i}`;
+
     const keyframes = `
         @keyframes ${animationName} {
             from {
-                transform: translate(0, 0);
+                transform: translate(${xStart}px, ${yStart}px);
             }
             to {
-                transform: translate(50px, 50px);
+                transform: translate(${xEnd}px, ${yEnd}px);
             }
         }
     `;
@@ -19,15 +24,12 @@ for (let i = 0; i < 250; i++) {
     style.innerHTML = keyframes;
     document.head.appendChild(style);
 
-    // Adjust the random position to stay within the visible area
-    const left = Math.random() * (window.innerWidth - 10) + 5;
-    const top = Math.random() * (window.innerHeight - 10) + 5;
+    dot.style.left = `${Math.random() * 100}vw`;
+    dot.style.top = `${Math.random() * 100}vh`;
+    dot.style.animation = `${animationName} 2.5s infinite alternate`; // Halved the animation time to 2.5s
+    dot.style.animationDelay = `${Math.random() * 5}s`;
 
-    dot.style.left = `${left}px`;
-    dot.style.top = `${top}px`;
-    dot.style.animation = `${animationName} 2.5s infinite alternate`;
-
-    document.querySelector('.dots').appendChild(dot);
+    document.body.appendChild(dot);
 }
 
 function copyToClipboard() {
