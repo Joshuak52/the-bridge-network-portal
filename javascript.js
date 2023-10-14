a372d20017e6097dbd8d9bd10a232167096c4937
Joshuak52 <joshuak5222@gmail.com>
2023-10-14 16:06:59 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,27 @@
 for (let i = 0; i < 250; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
-    dot.style.setProperty('--delay', Math.random() * 5); // Add animation delay
+
+    const animationName = `floatDot${i}`;
+    const keyframes = `
+        @keyframes ${animationName} {
+            from {
+                transform: translate(0, 0);
+            }
+            to {
+                transform: translate(50px, 50px);
+            }
+        }
+    `;
+
+    const style = document.createElement('style');
+    style.innerHTML = keyframes;
+    document.head.appendChild(style);
 
     dot.style.left = `${Math.random() * 100}vw`;
     dot.style.top = `${Math.random() * 100}vh`;
+    dot.style.animation = `${animationName} 2.5s infinite alternate`; // Apply animation
+    dot.style.animationDelay = `${Math.random() * 5}s`;
 
     document.querySelector('.dots').appendChild(dot);
 }
