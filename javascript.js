3178012869037d6f7cb22281291b6b7c5a79d1f3
Joshuak52 <joshuak5222@gmail.com>
2023-10-14 16:51:25 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,34 +2,12 @@
 for (let i = 0; i < 250; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
-
-    const animationName = `floatDot${i}`;
-    const keyframes = `
-        @keyframes ${animationName} {
-            from {
-                transform: translate(0, 0);
-            }
-            to {
-                transform: translate(50px, 50px);
-            }
-        }
-    `;
-
-    const style = document.createElement('style');
-    style.innerHTML = keyframes;
-    document.head.appendChild(style);
-
-    dot.style.left = `${Math.random() * 100}vw`;
-    dot.style.top = `${Math.random() * 100}vh`;
-    dot.style.animation = `${animationName} 2.5s infinite alternate`; // Apply animation
-    dot.style.animationDelay = `${Math.random() * 5}s`;
-
+    
+    const left = Math.random() * window.innerWidth;
+    const top = Math.random() * window.innerHeight;
+    
+    dot.style.left = `${left}px`;
+    dot.style.top = `${top}px`;
+    
     document.querySelector('.dots').appendChild(dot);
-}
-
-function copyToClipboard() {
-    const ipBox = document.getElementById("serverIp");
-    ipBox.select();
-    document.execCommand("copy");
-    alert("Server IP copied to clipboard!");
 }
