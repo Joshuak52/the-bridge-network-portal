0390f3cb6f45fc1a6c485bfc0bc5f21479aa7080
Joshuak52 <joshuak5222@gmail.com>
2023-10-14 14:22:19 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ for (let i = 0; i < 250; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
 
-    const xStart = (Math.random() - 0.5) * 100;
-    const yStart = (Math.random() - 0.5) * 100;
-    const xEnd = (Math.random() - 0.5) * 100;
-    const yEnd = (Math.random() - 0.5) * 100;
+    const xStart = (Math.random() - 0.5) * 100; // Random X start
+    const yStart = (Math.random() - 0.5) * 100; // Random Y start
+    const xEnd = (Math.random() - 0.5) * 100; // Random X end
+    const yEnd = (Math.random() - 0.5) * 100; // Random Y end
 
     const animationName = `floatDot${i}`;
 
@@ -26,30 +26,15 @@ for (let i = 0; i < 250; i++) {
 
     dot.style.left = `${Math.random() * 100}vw`;
     dot.style.top = `${Math.random() * 100}vh`;
-    dot.style.animation = `${animationName} 2.5s infinite alternate`;
+    dot.style.animation = `${animationName} 2.5s infinite alternate`; // Halved the animation time to 2.5s
     dot.style.animationDelay = `${Math.random() * 5}s`;
 
     document.body.appendChild(dot);
 }
 
-function copyAndLaunch() {
+function copyToClipboard() {
     const ipBox = document.getElementById("serverIp");
     ipBox.select();
     document.execCommand("copy");
     alert("Server IP copied to clipboard!");
-
-    window.location.href = `minecraftpe:?server=${ipBox.value}`;
 }
-
-
-const isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.iOS());
-    }
-};
