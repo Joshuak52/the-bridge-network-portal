b75c654e7bb073bbadea2d98f55637bb9236f928
Joshuak52 <joshuak5222@gmail.com>
2023-10-26 17:09:54 -0400

    Update and rename javascript.js to script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var survcartConfig = {
+    "id": "u4OxzEBKXaxMg3SyCfOw",
+    "companyId": "C3bTb8obBQZpDYiB0wYE",
+    "selectors": [
+        {
+            "configId": "q5fMvqVQZSxLyYStQteo",
+            "id": "7nKjlxlqv9XHdeJeQufD",
+            "ga4_tags": [],
+            "default": true,
+            "workflowId": "IZ6IxuzPNSnDVw0xQlRS",
+            "classes": "survcart-embed-presenter",
+            "tokens": [
+                "Book Now",
+                "Book Now!",
+                "BOOK NOW"
+            ]
+        }
+    ],
+    "embedName": "Embed Configuration 1",
+    "staged": false
+};
+
+// Additional JavaScript can go here
