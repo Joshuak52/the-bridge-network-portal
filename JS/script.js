93c53231d16f1e29550faff9e3ff8a00b3cceb11
Joshuak52 <joshuak5222@gmail.com>
2024-07-27 17:58:10 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,16 @@
 // Script regarding players, using MCAPI - https://mcapi.us/
 window.addEventListener('load', function theseFunc() {
-    MinecraftAPI.getServerStatus('thebridgemc.net', {
-        port: 25565
-    }, function (err, status) {
-        if (err) {
-            return document.querySelector('.players').innerHTML = 'Error...';
-        }
-        document.querySelector('.players').innerHTML = status.players.now;
-    });
-    document.querySelector('.copyIP').addEventListener('click', function copyIP() {
-        console.log('Copying the IP');
-        var clipboard = new ClipboardJS('.copyIP');
-        alertify.success('IP COPIED!');
-    });
+        MinecraftAPI.getServerStatus('TheBridgeMC.net', {
+                port: 25565
+        }, function (err, status) {
+                if (err) {
+                        return document.querySelector('.players').innerHTML = 'Error...';
+                }
+                document.querySelector('.players').innerHTML = status.players.now;
+        });
+        document.querySelector('.copyIP').addEventListener('click', function copyIP() {
+                console.log('Copying the IP');
+                var clipboard = new ClipboardJS('.copyIP');
+                alertify.success('IP COPIED!');
+        });
 });
